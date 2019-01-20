ea766707f6c92bf58aee1ef98860c3947fb441a6
Oliver 2wist <joliver017@gmail.com>
2019-01-20 00:21:28 -0600

    code keeps track of correct, incorrect, and unanswered questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,35 @@
 $(document).ready(function() {
   var correctAns = 0;
   var wrongAns = 0;
-  var timer = 30;
   var noAns = 0;
+  var timer = 30;
 
   var questions = [
     {
       question: "What is the water type starter from Generation 1?",
       choice: ["Squirtle", "Charmander", "Pikachu", "Bulbasaur"],
       answer: 0,
-      photo: "assets/images/pupusas.jpg"
     },
     {
       question: "What is the name of the nurse at the Pokemon Center?",
       choice: ["Jenny", "Joan", "Joy", "Jessie"],
       answer: 2,
-      photo: "assets/images/mtdew.gif"
-    }
+    },
+    {
+      question: "What town is Ash from?",
+      choice: ["Vermilion", "Pallet", "Cerulean", "Pewter"],
+      answer: 1,
+    },
+    {
+      question: "Who is the gym leader in Pewter City?",
+      choice: ["Oak", "Gary", "Misty", "Brock"],
+      answer: 3,
+    },
+    {
+      question: "How many moves can a Pokemon learn?",
+      choice: ["4", "5", "6", "3"],
+      answer: 0,
+    },
   ];
 
   $("#startBtn").on("click", function() {
@@ -67,6 +80,75 @@ $(document).ready(function() {
         "<br>"
     );
 
+    $("#question").append(questions[2].question + "<br>");
+    $("#question").append(
+      "<input name='q3' type=radio value='notAns'>" +
+        questions[2].choice[0] +
+        "<br>"
+    );
+    $("#question").append(
+      "<input name='q3' type=radio value='ans'>" +
+        questions[2].choice[1] +
+        "<br>"
+    );
+    $("#question").append(
+      "<input name='q3' type=radio value='notAns'>" +
+        questions[2].choice[2] +
+        "<br>"
+    );
+    $("#question").append(
+      "<input name='q3' type=radio value='notAns'>" +
+        questions[2].choice[3] +
+        "<br>" +
+        "<br>"
+    );
+
+    $("#question").append(questions[3].question + "<br>");
+    $("#question").append(
+      "<input name='q4' type=radio value='notAns'>" +
+        questions[3].choice[0] +
+        "<br>"
+    );
+    $("#question").append(
+      "<input name='q4' type=radio value='notAns'>" +
+        questions[3].choice[1] +
+        "<br>"
+    );
+    $("#question").append(
+      "<input name='q4' type=radio value='notAns'>" +
+        questions[3].choice[2] +
+        "<br>"
+    );
+    $("#question").append(
+      "<input name='q4' type=radio value='ans'>" +
+        questions[3].choice[3] +
+        "<br>" +
+        "<br>"
+    );
+
+    $("#question").append(questions[4].question + "<br>");
+    $("#question").append(
+      "<input name='q5' type=radio value='ans'>" +
+        questions[4].choice[0] +
+        "<br>"
+    );
+    $("#question").append(
+      "<input name='q5' type=radio value='notAns'>" +
+        questions[4].choice[1] +
+        "<br>"
+    );
+    $("#question").append(
+      "<input name='q5' type=radio value='notAns'>" +
+        questions[4].choice[2] +
+        "<br>"
+    );
+    $("#question").append(
+      "<input name='q5' type=radio value='notAns'>" +
+        questions[4].choice[3] +
+        "<br>" +
+        "<br>"
+    );
+
     $("#divBtn").append("<button id='doneBtn'> Done </button>");
     submitDone();
   });
@@ -76,21 +158,22 @@ $(document).ready(function() {
     $('input[type="radio"]:checked').each(function() {
       if ($(this).val() == "ans") {
         correctAns++;
-        console.log("Correct: " + correctAns);
-        // console.log(this) This referring to the radio button
+        noAns = noAns -3; // The function below is counting ALL unselected radio options, so on the questions where an option is selected, subtract the 3 unselected options
+        console.log("Correct: " + correctAns); // console.log(this) This referring to the radio button
       } 
       else if ($(this).val() == "notAns") {
         wrongAns++;
+        noAns = noAns - 3;
         console.log("Incorrect: " + wrongAns);
       }
     });
 
-    
-        if ($('input[type="radio"]').is(":checked") == false) {
+    $('input[type="radio"]').each(function() {
+        if ($(this).is(":checked") == false) {
           noAns++;
           console.log("Unanswered: " + noAns);
         }
-      
+      });
 
     results();
     
@@ -102,6 +185,7 @@ $(document).ready(function() {
     $("#doneBtn").hide();
     $("#results").append("<p> Correct: " + correctAns + "</p>")
     $("#results").append("<p> Incorrect: " + wrongAns + "</p>")
-    $("#results").append("<p> Unanswered: " + noAns + "</p>")
+    $("#results").append("<p> Unanswered: " + noAns / 4 + "</p>")
+    // Unanswered score is divided by 4 for the number of options unselected in each question
   }
 });
